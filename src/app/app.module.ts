a8bd74519a5d19273c971ea483e8d5055f0db9b5
santoshpavan <santoshpavan.psp@gmail.com>
2021-01-09 16:20:17 -0500

    adding required modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    CurrentTrainingComponent
   ],
   imports: [
     BrowserModule
