778e4d4235bae044912c1b64eb322111b8bb4a17
santoshpavan <santoshpavan.psp@gmail.com>
2021-01-14 01:21:53 -0500

    pagination past training

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../exercise.model';
@@ -17,6 +18,7 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
 
   // ViewChild gives access to components in the view
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private trainingService: TrainingService) { }
 
@@ -27,6 +29,7 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
   // runs after the view is rendered
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   doFilter(filterValue: string) {
