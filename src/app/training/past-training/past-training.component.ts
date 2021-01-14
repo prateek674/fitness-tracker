bea2536873261c4d42845b302a0f6021341ad5a1
santoshpavan <santoshpavan.psp@gmail.com>
2021-01-14 01:10:05 -0500

    sorting past training

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
@@ -8,15 +9,23 @@ import { TrainingService } from '../training.service';
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumsn = ['date', 'name', 'duration', 'calories', 'state'];
   // MatTableDataSource always expects an array of data coming in.
   // So, expecting Exercise[] but we don't have to mention that because of MatTableDataSource
   dataSource = new MatTableDataSource<Exercise>();
 
+  // ViewChild gives access to components in the view
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
     this.dataSource.data = this.trainingService.getExercises();
+  }
+
+  // runs after the view is rendered
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 }
