4d15058cf9429f83f4e9947bffd80c983b78f733
santoshpavan <santoshpavan.psp@gmail.com>
2021-01-19 07:04:13 -0500

    extracting basic data from firebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -4,6 +4,7 @@ import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -13,7 +14,7 @@ import { TrainingService } from '../training.service';
 export class NewTrainingComponent implements OnInit {
   // @Output() trainingStart = new EventEmitter<void>();
   // exercises: Exercise[] = [];
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
@@ -21,7 +22,20 @@ export class NewTrainingComponent implements OnInit {
     // this.exercises = this.trainingService.getAvailableExercises();
     // valueChanges gives the Observable that we subscribe to
     // valueChanges strips off the metadata like the ID
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    // this.exercises = this.db.collection('availableExercises').valueChanges();
+    
+    //snapshotChanges will give the ID as well
+    this.exercises = this.db
+        .collection('availableExercises')
+        .snapshotChanges()
+        .map(docArray => {
+          return docArray.map(doc => {
+            return {
+              id: doc.payload.doc.id,
+              ...doc.payload.doc.data()
+            };
+          });
+        });
   }
 
   onStartTraining(form: NgForm) {
