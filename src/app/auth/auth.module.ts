ff9a3baaa391b946391dfd2265094df66c30719c
santoshpavan <santoshpavan.psp@gmail.com>
2021-01-25 17:08:48 -0500

    splitting the routing into appropriate modules

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AngularFireAuthModule } from "angularfire2/auth";
 import { SharedModule } from "../shared/shared.module";
+import { AuthRoutingModule } from "./auth-routing-module";
 
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
@@ -19,7 +20,8 @@ import { SignupComponent } from "./signup/signup.component";
     imports: [
         ReactiveFormsModule,
         AngularFireAuthModule,
-        SharedModule
+        SharedModule,
+        AuthRoutingModule
     ],
     exports: []
 })
