d51199d51478acde18785121f897047fc439eede
santoshpavan <santoshpavan.psp@gmail.com>
2021-02-04 18:49:34 -0500

    training reducer using in new-training component

--- src/app/training/training.reducer.ts
+++ src/app/training/training.reducer.ts
@@ -59,3 +59,5 @@ export const getTrainingState = createFeatureSelector<TrainingState>('training')
 export const getAvailableExercises = createSelector(getTrainingState, (state: TrainingState) => (state.availableExercises));
 export const getFinishedExercises = createSelector(getTrainingState, (state: TrainingState) => (state.finishedExercises));
 export const getTrainingExercises = createSelector(getTrainingState, (state: TrainingState) => (state.activeExercise));
+
+export const getIsTraining = createSelector(getTrainingState, (state: TrainingState) =>  (state.activeExercise != null));
